2c985ad9dba535378e54dee102e9e956c4d1c16b
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:23:13 -0600

    Create login file

---
+++ src/iniciarSesion.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <mostrar-formulario>
+        <h3 slot="titulo">Iniciar sesión</h3>
+        <div slot="elementos">
+          <div class="col-auto">
+            <label for="correo">Correo electrónico</label>
+            <input type="email" class="form-control" placeholder="Escribe tu correo" id="correo">
+          </div>
+          <div class="col-auto">
+            <label for="clave">Contraseña</label>
+            <input type="password" class="form-control" id="clave" placeholder="Escribe tu contraseña">
+          </div>
+        </div>
+        <div slot="boton" class="d-grid gap-2 col-6 mx-auto">
+            <button class="btn btn-primary mb-3">Iniciar sesión</button>
+        </div>
+      </mostrar-formulario>
+    </div>
+  </div>
+</template>
+
+<script>
+import MostrarFormulario from './mostrarFormulario.vue'
+export default {
+  components: {
+    mostrarFormulario:MostrarFormulario
+  }
+}
+</script>
