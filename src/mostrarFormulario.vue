4f1289e326ebd7205d41935514b3a005d598fe3d
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:24:39 -0600

    Create form template file

---
+++ src/mostrarFormulario.vue
@@ -0,0 +1,17 @@
+<template>
+    <form>
+        <slot name="titulo"></slot>
+        <slot name="elementos"></slot>
+        <slot name="boton"></slot>
+    </form>
+</template>
+
+<script>
+export default {
+   components: {
+  }
+}
+</script>
+
+<style>
+</style>
